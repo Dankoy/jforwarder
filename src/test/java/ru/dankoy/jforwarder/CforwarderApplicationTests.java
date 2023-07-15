b4e597e4a4ae02e18804e27572369c6cdf88003b
Evgeny <transingularity@gmail.com>
2023-07-15 08:28:31 +0300

    Initial commit

---
+++ src/test/java/ru/dankoy/jforwarder/CforwarderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dankoy.jforwarder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CforwarderApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
