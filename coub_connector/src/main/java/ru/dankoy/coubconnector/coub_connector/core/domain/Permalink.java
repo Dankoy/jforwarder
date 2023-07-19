ebb4c3c79c95a5c9873d4328995d92e43bbb33a8
Evgeny <transingularity@gmail.com>
2023-07-19 12:54:24 +0300

    refactor: removed coub_connector microservice

--- coub_connector/src/main/java/ru/dankoy/coubconnector/coub_connector/core/domain/Permalink.java
+++
@@ -1,9 +0,0 @@
-package ru.dankoy.coubconnector.coub_connector.core.domain;
-
-public interface Permalink {
-
-  String getPermalink();
-
-  void setUrl(String url);
-
-}
--- coub_connector/src/main/java/ru/dankoy/coubconnector/coub_connector/core/service/coub/CoubService.java
+++
@@ -1,9 +0,0 @@
-package ru.dankoy.coubconnector.coub_connector.core.service.coub;
-
-import ru.dankoy.coubconnector.coub_connector.core.domain.coubcom.coub.CoubWrapper;
-
-public interface CoubService {
-
-  CoubWrapper getCoubsWrapperForCommunityAndSection(String communityName, String sectionName,
-      int page);
-}
