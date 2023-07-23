4fda27000f83660c42a52741c5669c98cfa5f7ad
Evgeny <transingularity@gmail.com>
2023-07-23 13:53:16 +0300

    refactor: updated feign for message producer to

--- t_coubs_initiator/src/main/java/ru/dankoy/tcoubsinitiator/core/feign/messageproducer/MessageProducerFeign.java
+++ t_coubs_initiator/src/main/java/ru/dankoy/tcoubsinitiator/core/feign/messageproducer/MessageProducerFeign.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import ru.dankoy.tcoubsinitiator.core.domain.subscribtionsholder.subscription.Subscription;
 
-@FeignClient(name = "message-producer")
+@FeignClient(name = "kafka-message-producer")
 public interface MessageProducerFeign {
 
-  @GetMapping(path = "/api/v1/produce")
+  @GetMapping(path = "/api/v1/subscriptions")
   void sendSubscriptions(@RequestBody List<Subscription> subscriptions);
 
 }
