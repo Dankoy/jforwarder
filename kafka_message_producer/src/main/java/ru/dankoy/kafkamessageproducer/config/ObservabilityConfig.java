856e476175342f8b170fd26071fcaae5802a8148
Evgeny <transingularity@gmail.com>
2023-07-26 12:29:05 +0300

    refactor: added zipkin tracing integration

---
+++ kafka_message_producer/src/main/java/ru/dankoy/kafkamessageproducer/config/ObservabilityConfig.java
@@ -0,0 +1,18 @@
+package ru.dankoy.kafkamessageproducer.config;
+
+
+import feign.Capability;
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObservabilityConfig {
+
+  @Bean
+  public Capability capability(final MeterRegistry registry) {
+    return new MicrometerCapability(registry);
+  }
+
+}
