72c50fddb9e043f1253f0a47fb5b81d4624e3f8b
Evgeny <transingularity@gmail.com>
2023-07-26 08:35:58 +0300

    fix: fixed feign client

--- t_coubs_initiator/src/main/java/ru/dankoy/tcoubsinitiator/core/feign/subscription/SubscriptionFeign.java
+++ t_coubs_initiator/src/main/java/ru/dankoy/tcoubsinitiator/core/feign/subscription/SubscriptionFeign.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.dankoy.tcoubsinitiator.core.domain.subscribtionsholder.subscription.Subscription;
 
 @FeignClient(name = "subscriptions-holder")
@@ -19,6 +20,6 @@ public interface SubscriptionFeign {
   List<Subscription> getAllSubscriptions();
 
   @GetMapping(path = "/api/v1/subscriptions", params = {"active"})
-  List<Subscription> getAllSubscriptionsWithActiveChats(boolean active);
+  List<Subscription> getAllSubscriptionsWithActiveChats(@RequestParam boolean active);
 
 }
