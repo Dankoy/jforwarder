2117644dff842bf756c1d08159a76311c9ecda12
Evgeny <transingularity@gmail.com>
2023-07-28 12:08:48 +0300

    refactor: updated tag dto

--- coub_tags_searcher/src/main/java/ru/dankoy/coubtagssearcher/core/dto/TagDTO.java
+++ coub_tags_searcher/src/main/java/ru/dankoy/coubtagssearcher/core/dto/TagDTO.java
@@ -2,9 +2,11 @@ package ru.dankoy.coubtagssearcher.core.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.ToString;
 import ru.dankoy.coubtagssearcher.core.domain.Tag;
 
+@Data
 @ToString
 @AllArgsConstructor
 public class TagDTO {
