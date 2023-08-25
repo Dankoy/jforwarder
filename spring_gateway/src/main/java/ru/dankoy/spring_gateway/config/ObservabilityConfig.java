78a6b5eeab4c16563429ad372b77029542236ea9
Evgeny <transingularity@gmail.com>
2023-08-25 08:50:56 +0300

    Zipkin ignore actuator requests (#30)
    * refactor: removed depends on healthy zipkin cassandra
    
    * fix: ignore actuator in zipkin traces
    
    * chore: incremented project version

--- spring_gateway/src/main/java/ru/dankoy/spring_gateway/config/ObservabilityConfig.java
+++ spring_gateway/src/main/java/ru/dankoy/spring_gateway/config/ObservabilityConfig.java
@@ -4,7 +4,7 @@ import io.micrometer.observation.ObservationRegistry;
 import org.springframework.boot.actuate.autoconfigure.observation.ObservationRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.server.observation.ServerRequestObservationContext;
+import org.springframework.http.server.reactive.observation.ServerRequestObservationContext;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.util.PathMatcher;
 
@@ -14,9 +14,11 @@ public class ObservabilityConfig {
   @Bean
   ObservationRegistryCustomizer<ObservationRegistry> skipActuatorEndpointsFromObservation() {
     PathMatcher pathMatcher = new AntPathMatcher("/");
-    return (registry) -> registry.observationConfig().observationPredicate((name, context) -> {
+    return registry -> registry.observationConfig().observationPredicate((name, context) -> {
+      // ServerRequestObservationContext is reactive. So import reactive dep
       if (context instanceof ServerRequestObservationContext observationContext) {
-        return !pathMatcher.match("/actuator/**", observationContext.getCarrier().getRequestURI());
+        return !pathMatcher.match("/actuator/**",
+            observationContext.getCarrier().getURI().getPath());
       } else {
         return true;
       }
@@ -25,7 +27,7 @@ public class ObservabilityConfig {
 
   @Bean
   ObservationRegistryCustomizer<ObservationRegistry> skipSecuritySpansFromObservation() {
-    return (registry) -> registry.observationConfig().observationPredicate((name, context) ->
+    return registry -> registry.observationConfig().observationPredicate((name, context) ->
         !name.startsWith("spring.security"));
   }
 
