82c94a389f3294f390a3d7d2ef10f0dd4f979306
Evgeny <transingularity@gmail.com>
2024-01-29 16:05:08 +0300

    refactor: created new repo for inherited tag subscriptions

---
+++ subscriptions_holder/src/main/java/ru/dankoy/subscriptionsholder/subscriptions_holder/core/repository/TagSubRepository.java
@@ -0,0 +1,39 @@
+package ru.dankoy.subscriptionsholder.subscriptions_holder.core.repository;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import ru.dankoy.subscriptionsholder.subscriptions_holder.core.domain.subscriptions.TagSub;
+
+public interface TagSubRepository extends JpaRepository<TagSub, Long> {
+
+  @EntityGraph(value = "tag-subscription-full-inherited", type = EntityGraphType.LOAD)
+  @Override
+  Page<TagSub> findAll(Pageable pageable);
+
+  @EntityGraph(value = "tag-subscription-full-inherited", type = EntityGraphType.LOAD)
+  Page<TagSub> findAllByChatActive(@Param("active") boolean active, Pageable pageable);
+
+
+  @EntityGraph(value = "tag-subscription-full-inherited", type = EntityGraphType.LOAD)
+  List<TagSub> getAllByChatChatId(@Param("telegramChatId") long telegramChatId);
+
+
+  @EntityGraph(value = "tag-subscription-full-inherited", type = EntityGraphType.LOAD)
+  List<TagSub> getAllByChatChatIdAndTagTitle(
+      @Param("externalChatId") long externalChatId,
+      @Param("tagTitle") String tagTitle);
+
+
+  @EntityGraph(value = "tag-subscription-full-inherited", type = EntityGraphType.LOAD)
+  Optional<TagSub> getByChatChatIdAndTagTitleAndOrderName(
+      @Param("externalChatId") long externalChatId,
+      @Param("tagTitle") String tagTitle,
+      @Param("orderName") String orderName);
+
+}
