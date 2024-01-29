a83b7c0bbcdb4dd78c0c4e81bbc916c3952209cf
Evgeny <transingularity@gmail.com>
2024-01-29 12:19:43 +0300

    refactor: added dtos for inherited community subscriptions

---
+++ subscriptions_holder/src/main/java/ru/dankoy/subscriptionsholder/subscriptions_holder/core/dto/communitysub/CommunitySubCreateDTO.java
@@ -0,0 +1,62 @@
+package ru.dankoy.subscriptionsholder.subscriptions_holder.core.dto.communitysub;
+
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import ru.dankoy.subscriptionsholder.subscriptions_holder.core.domain.CommunitySubscription;
+import ru.dankoy.subscriptionsholder.subscriptions_holder.core.domain.subscriptions.CommunitySub;
+import ru.dankoy.subscriptionsholder.subscriptions_holder.core.dto.SectionDTO;
+
+@ToString
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommunitySubCreateDTO {
+
+  @Valid
+  @NotNull
+  private CommunitySubCreateCommunityDTO community;
+
+  @Valid
+  @NotNull
+  private SectionDTO section;
+
+  @Valid
+  @NotNull
+  private ChatCreateDTO chat;
+
+  private String lastPermalink;
+
+
+  public static CommunitySubCreateDTO toDTO(CommunitySub communitySubscription) {
+
+    return CommunitySubCreateDTO.builder()
+        .community(CommunitySubCreateCommunityDTO.toDTO(communitySubscription.getCommunity()))
+        .section(SectionDTO.toDTO(communitySubscription.getSection()))
+        .chat(
+            ChatCreateDTO.toDTO(communitySubscription.getChat()))
+        .lastPermalink(communitySubscription.getLastPermalink())
+        .build();
+
+  }
+
+  public static CommunitySub fromDTO(CommunitySubCreateDTO dto) {
+
+    return CommunitySub.builder()
+        .id(0)
+        .community(CommunitySubCreateCommunityDTO.fromDTO(dto.getCommunity()))
+        .section(SectionDTO.fromDTO(dto.getSection()))
+        .chat(ChatCreateDTO.fromDTO(dto.getChat()))
+        .lastPermalink(dto.getLastPermalink())
+        .build();
+
+  }
+
+
+}
---
+++ subscriptions_holder/src/main/java/ru/dankoy/subscriptionsholder/subscriptions_holder/core/dto/communitysub/CommunitySubUpdatePermalinkDTO.java
@@ -0,0 +1,63 @@
+package ru.dankoy.subscriptionsholder.subscriptions_holder.core.dto.communitysub;
+
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import ru.dankoy.subscriptionsholder.subscriptions_holder.core.domain.subscriptions.CommunitySub;
+import ru.dankoy.subscriptionsholder.subscriptions_holder.core.dto.SectionDTO;
+
+@ToString
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommunitySubUpdatePermalinkDTO {
+
+  @Valid
+  @NotNull
+  private CommunitySubCreateCommunityDTO community;
+
+  @Valid
+  @NotNull
+  private SectionDTO section;
+
+  @Valid
+  @NotNull
+  private ChatCreateDTO chat;
+
+  @NotEmpty
+  private String lastPermalink;
+
+
+  public static CommunitySubUpdatePermalinkDTO toDTO(CommunitySub communitySubscription) {
+
+    return CommunitySubUpdatePermalinkDTO.builder()
+        .community(CommunitySubCreateCommunityDTO.toDTO(communitySubscription.getCommunity()))
+        .section(SectionDTO.toDTO(communitySubscription.getSection()))
+        .chat(
+            ChatCreateDTO.toDTO(communitySubscription.getChat()))
+        .lastPermalink(communitySubscription.getLastPermalink())
+        .build();
+
+  }
+
+  public static CommunitySub fromDTO(CommunitySubUpdatePermalinkDTO dto) {
+
+    return CommunitySub.builder()
+        .id(0)
+        .community(CommunitySubCreateCommunityDTO.fromDTO(dto.getCommunity()))
+        .section(SectionDTO.fromDTO(dto.getSection()))
+        .chat(ChatCreateDTO.fromDTO(dto.getChat()))
+        .lastPermalink(dto.getLastPermalink())
+        .build();
+
+  }
+
+
+}
