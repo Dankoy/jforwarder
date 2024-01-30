a2358a27e944a10962ece3efc23d306a8607a51f
Evgeny <transingularity@gmail.com>
2024-01-30 09:26:24 +0300

    refactor: added graph

--- subscriptions_holder/src/main/java/ru/dankoy/subscriptionsholder/subscriptions_holder/core/domain/registry/SentCoubsRegistry.java
+++ subscriptions_holder/src/main/java/ru/dankoy/subscriptionsholder/subscriptions_holder/core/domain/registry/SentCoubsRegistry.java
@@ -8,6 +8,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedAttributeNode;
+import jakarta.persistence.NamedEntityGraph;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
@@ -16,6 +18,14 @@ import lombok.NoArgsConstructor;
 import ru.dankoy.subscriptionsholder.subscriptions_holder.core.domain.subscriptions.Subscription;
 
 
+@NamedEntityGraph(name = "sent-coubs-registry-full",
+    includeAllAttributes = true,
+    attributeNodes = {
+        @NamedAttributeNode("subscription"),
+        @NamedAttributeNode("coubPermalink"),
+        @NamedAttributeNode("dateTime")
+    }
+)
 @Table(name = "sent_coubs_registry")
 @Entity
 @Data
@@ -28,14 +38,9 @@ public class SentCoubsRegistry {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
-  //Нужно хранить по регистру для разных типов подписок, либо вынести общую часть подписок в
-  // отдельный класс и хранить в нем id, chat, lastPermalink так как они общие для всех подписок.
-  // Таким образом можно связать один архив для любых подписок. А добавление новых подписок
-  // будет простым полиморфизмом от родительского класса.
-
   @ManyToOne
   @JoinColumn(name = "subscription_id")
-  private Subscription subscription; // у разных типов подписок разная таблица референса, нобходимо наследование
+  private Subscription subscription;
 
   @Column(name = "coub_permalink")
   private String coubPermalink;
