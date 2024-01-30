d0dddfb6d7c1234e26f678c1c091bf8042d2b3fc
Evgeny <transingularity@gmail.com>
2024-01-30 15:03:27 +0300

    refactor: added regisrty request to feign

--- kafka_message_producer/src/main/java/ru/dankoy/kafkamessageproducer/core/feign/subscriptionsholder/subscription/SubscriptionFeign.java
+++ kafka_message_producer/src/main/java/ru/dankoy/kafkamessageproducer/core/feign/subscriptionsholder/subscription/SubscriptionFeign.java
@@ -2,9 +2,11 @@ package ru.dankoy.kafkamessageproducer.core.feign.subscriptionsholder.subscripti
 
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import ru.dankoy.kafkamessageproducer.core.domain.subscription.CommunitySubscription;
+import ru.dankoy.kafkamessageproducer.core.domain.regisrty.SentCoubsRegistry;
+import ru.dankoy.kafkamessageproducer.core.domain.subscription.communitysubscription.CommunitySubscription;
 import ru.dankoy.kafkamessageproducer.core.domain.subscription.tagsubscription.TagSubscription;
 
 @FeignClient(name = "subscriptions-holder")
@@ -19,5 +21,7 @@ public interface SubscriptionFeign {
   TagSubscription updateTagSubscriptionPermalink(
       @RequestBody TagSubscription tagSubscription);
 
+  @PostMapping(path = "/api/v1/sent_coubs_registry")
+  SentCoubsRegistry createRegistryEntry(@RequestBody SentCoubsRegistry sentCoubsRegistry);
 
 }
