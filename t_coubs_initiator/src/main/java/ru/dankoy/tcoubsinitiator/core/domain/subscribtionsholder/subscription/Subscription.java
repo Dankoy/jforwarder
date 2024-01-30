bde0e57c5ea8fe7ea3f6bbf57e4f08b59ba50465
Evgeny <transingularity@gmail.com>
2024-01-30 12:15:47 +0300

    refactor: moved common data to parent

--- t_coubs_initiator/src/main/java/ru/dankoy/tcoubsinitiator/core/domain/subscribtionsholder/subscription/Subscription.java
+++ t_coubs_initiator/src/main/java/ru/dankoy/tcoubsinitiator/core/domain/subscribtionsholder/subscription/Subscription.java
@@ -1,10 +1,14 @@
 package ru.dankoy.tcoubsinitiator.core.domain.subscribtionsholder.subscription;
 
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
+import ru.dankoy.tcoubsinitiator.core.domain.coubcom.coub.Coub;
 import ru.dankoy.tcoubsinitiator.core.domain.subscribtionsholder.communitysubscription.Chat;
 
 @EqualsAndHashCode
@@ -19,5 +23,25 @@ public class Subscription {
   private Chat chat;
 
   private String lastPermalink;
+
+  @Setter
+  private List<Coub> coubs = new ArrayList<>();
+
+  public void addCoubs(List<Coub> coubsToAdd) {
+
+    for (var coub : coubsToAdd) {
+
+      var newCoub = new Coub(
+          coub.getId(),
+          coub.getTitle(),
+          coub.getPermalink(),
+          coub.getUrl(),
+          coub.getPublishedAt()
+      );
+      coubs.add(newCoub);
+
+    }
+
+  }
 
 }
--- t_coubs_initiator/src/main/java/ru/dankoy/tcoubsinitiator/core/domain/subscribtionsholder/tagsubscription/TagSubscription.java
+++ t_coubs_initiator/src/main/java/ru/dankoy/tcoubsinitiator/core/domain/subscribtionsholder/tagsubscription/TagSubscription.java
@@ -1,15 +1,11 @@
 package ru.dankoy.tcoubsinitiator.core.domain.subscribtionsholder.tagsubscription;
 
 
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
-import ru.dankoy.tcoubsinitiator.core.domain.coubcom.coub.Coub;
 import ru.dankoy.tcoubsinitiator.core.domain.subscribtionsholder.subscription.Subscription;
 
 @EqualsAndHashCode(callSuper = true)
@@ -27,24 +23,5 @@ public class TagSubscription extends Subscription {
 
   private Type type;
 
-  @Setter
-  private List<Coub> coubs = new ArrayList<>();
-
-  public void addCoubs(List<Coub> coubsToAdd) {
-
-    for (var coub : coubsToAdd) {
-
-      var newCoub = new Coub(
-          coub.getId(),
-          coub.getTitle(),
-          coub.getPermalink(),
-          coub.getUrl(),
-          coub.getPublishedAt()
-      );
-      coubs.add(newCoub);
-
-    }
-
-  }
 
 }
