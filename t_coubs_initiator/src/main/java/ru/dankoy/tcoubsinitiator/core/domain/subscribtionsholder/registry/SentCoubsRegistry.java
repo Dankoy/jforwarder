2f061bb20067c5b74c1ef43c4f4b1a703a1418d9
Evgeny <transingularity@gmail.com>
2024-01-30 12:01:35 +0300

    refactor: added sent coubs registry domain

---
+++ t_coubs_initiator/src/main/java/ru/dankoy/tcoubsinitiator/core/domain/subscribtionsholder/registry/SentCoubsRegistry.java
@@ -0,0 +1,27 @@
+package ru.dankoy.tcoubsinitiator.core.domain.subscribtionsholder.registry;
+
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import ru.dankoy.tcoubsinitiator.core.domain.subscribtionsholder.subscription.Subscription;
+
+@EqualsAndHashCode
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class SentCoubsRegistry {
+
+  private long id;
+
+  private Subscription subscription;
+
+  private String coubPermalink;
+
+  private LocalDateTime dateTime;
+
+}
