33c09293cd2afcbab76b81dc37a66288815f01aa
Evgeny <transingularity@gmail.com>
2024-02-02 11:01:15 +0300

    fix: removed data manipulation from iterable migration

--- subscriptions_holder/src/main/resources/db/migration/4.0/V4.1__2024_02_02_update_tag_orders_schema.sql
+++ subscriptions_holder/src/main/resources/db/migration/4.0/V4.1__2024_02_02_update_tag_orders_schema.sql
@@ -1,37 +1,4 @@
 alter table tag_orders
     add value varchar(15);
 
-update
-    tag_orders
-set value = 'popular'
-where id =
-      (select id
-       from tag_orders
-       where tag_orders."name" = 'newest_popular');
-
-update
-    tag_orders
-set value = 'top'
-where id =
-      (select id
-       from tag_orders
-       where tag_orders."name" = 'likes_count');
-
-update
-    tag_orders
-set value = 'views_count'
-where id =
-      (select id
-       from tag_orders
-       where tag_orders."name" = 'views_count');
-
-update
-    tag_orders
-set value = 'fresh'
-where id =
-      (select id
-       from tag_orders
-       where tag_orders."name" = 'newest');
-
-
 
