88950b266ac9427a931765f81c12ee7b6f1a262e
Evgeny <transingularity@gmail.com>
2024-02-08 14:40:49 +0300

    refactor: added generalized coub message consumer

---
+++ kafka_message_consumer/src/main/java/ru/dankoy/kafkamessageconsumer/core/service/consumer/CoubMessageConsumer.java
@@ -0,0 +1,11 @@
+package ru.dankoy.kafkamessageconsumer.core.service.consumer;
+
+import java.util.List;
+import ru.dankoy.kafkamessageconsumer.core.domain.message.CoubMessage;
+
+public interface CoubMessageConsumer {
+
+  void accept(List<? extends CoubMessage> value);
+
+  void accept(CoubMessage value);
+}
