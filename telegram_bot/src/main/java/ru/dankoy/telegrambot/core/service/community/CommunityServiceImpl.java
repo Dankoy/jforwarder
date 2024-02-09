3bf374f492a42b0de4599aec6b094a79eb635a6e
Evgeny <transingularity@gmail.com>
2024-02-09 13:06:43 +0300

    refactor: added method community by name

--- telegram_bot/src/main/java/ru/dankoy/telegrambot/core/service/community/CommunityServiceImpl.java
+++ telegram_bot/src/main/java/ru/dankoy/telegrambot/core/service/community/CommunityServiceImpl.java
@@ -1,11 +1,15 @@
 package ru.dankoy.telegrambot.core.service.community;
 
+import feign.FeignException.NotFound;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.dankoy.telegrambot.core.domain.subscription.Community;
 import ru.dankoy.telegrambot.core.feign.subscriptionsholder.SubscriptionsHolderFeign;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class CommunityServiceImpl implements CommunityService {
@@ -15,5 +19,14 @@ public class CommunityServiceImpl implements CommunityService {
   @Override
   public List<Community> getAll() {
     return subscriptionsHolderFeign.getAllCommunities();
+  }
+
+  @Override
+  public Optional<Community> getByName(String communityName) {
+    try {
+      return Optional.of(subscriptionsHolderFeign.getCommunityByName(communityName));
+    } catch (NotFound e) {
+      return Optional.empty();
+    }
   }
 }
