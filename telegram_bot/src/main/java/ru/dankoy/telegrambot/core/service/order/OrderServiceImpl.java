22015b333c9bd5152963301a3d5378be6d2d760a
Evgeny <transingularity@gmail.com>
2024-02-09 13:07:09 +0300

    refactor: added order by value method

--- telegram_bot/src/main/java/ru/dankoy/telegrambot/core/service/order/OrderServiceImpl.java
+++ telegram_bot/src/main/java/ru/dankoy/telegrambot/core/service/order/OrderServiceImpl.java
@@ -1,7 +1,7 @@
 package ru.dankoy.telegrambot.core.service.order;
 
-
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.dankoy.telegrambot.core.domain.tagsubscription.Order;
@@ -16,5 +16,14 @@ public class OrderServiceImpl implements OrderService {
   @Override
   public List<Order> findAll() {
     return subscriptionsHolderFeign.getAllTagOrders();
+  }
+
+  @Override
+  public Optional<Order> findByValue(String value) {
+    try {
+      return Optional.of(subscriptionsHolderFeign.getOrderByValue(value));
+    } catch (Exception e) {
+      return Optional.empty();
+    }
   }
 }
