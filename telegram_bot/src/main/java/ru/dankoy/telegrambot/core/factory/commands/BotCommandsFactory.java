422e6ee7bef3bf0fb8f03e40d99cbc2121e1ccb8
Evgeny <transingularity@gmail.com>
2024-02-10 11:25:58 +0300

    refactor: added commands factory

---
+++ telegram_bot/src/main/java/ru/dankoy/telegrambot/core/factory/commands/BotCommandsFactory.java
@@ -0,0 +1,29 @@
+package ru.dankoy.telegrambot.core.factory.commands;
+
+import java.util.List;
+import java.util.Locale;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import ru.dankoy.telegrambot.core.service.localization.LocalisationService;
+
+public interface BotCommandsFactory {
+
+  LocalisationService localisationService();
+
+  Locale locale();
+
+  List<BotCommand> allKnownCommands();
+
+  BotCommand communitiesCommand();
+
+  BotCommand helpCommand();
+
+  BotCommand mySubscriptionsCommand();
+
+  BotCommand startCommand();
+
+  BotCommand subscribeCommand();
+
+  BotCommand unsubscribeCommand();
+
+  BotCommand tagOrdersCommand();
+}
