e8bea944e857c8d7447818dd7838592e1565d066
Evgeny <transingularity@gmail.com>
2024-02-10 17:18:26 +0300

    refactor: added parse mode validator

---
+++ telegram_bot/src/main/java/ru/dankoy/telegrambot/core/dto/validator/ParseModeValidator.java
@@ -0,0 +1,16 @@
+package ru.dankoy.telegrambot.core.dto.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.util.Objects;
+import org.telegram.telegrambots.meta.api.methods.ParseMode;
+
+public class ParseModeValidator implements ConstraintValidator<ParseModeConstraint, String> {
+  @Override
+  public boolean isValid(String parseMode, ConstraintValidatorContext constraintValidatorContext) {
+    return Objects.nonNull(parseMode)
+        && (parseMode.equals(ParseMode.MARKDOWN)
+            || parseMode.equals(ParseMode.MARKDOWNV2)
+            || parseMode.equals(ParseMode.HTML));
+  }
+}
