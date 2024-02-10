bfeed389b9d5129532c9c0976d68203015c6fc67
Evgeny <transingularity@gmail.com>
2024-02-10 11:32:33 +0300

    refactor: updated commands

--- telegram_bot/src/main/java/ru/dankoy/telegrambot/core/service/bot/commands/MySubscriptionsCommand.java
+++ telegram_bot/src/main/java/ru/dankoy/telegrambot/core/service/bot/commands/MySubscriptionsCommand.java
@@ -1,15 +1,12 @@
 package ru.dankoy.telegrambot.core.service.bot.commands;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 
 @Slf4j
-@Component
 public class MySubscriptionsCommand extends BotCommand {
 
-  public MySubscriptionsCommand() {
-    super("my_subscriptions", "show my subscriptions");
+  public MySubscriptionsCommand(String command, String description) {
+    super(command, description);
   }
-
 }
--- telegram_bot/src/main/java/ru/dankoy/telegrambot/core/service/bot/commands/UnsubscribeCommand.java
+++ telegram_bot/src/main/java/ru/dankoy/telegrambot/core/service/bot/commands/UnsubscribeCommand.java
@@ -1,15 +1,12 @@
 package ru.dankoy.telegrambot.core.service.bot.commands;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 
 @Slf4j
-@Component
 public class UnsubscribeCommand extends BotCommand {
 
-  public UnsubscribeCommand() {
-    super("unsubscribe", "unsubscribe from community or tag");
+  public UnsubscribeCommand(String command, String description) {
+    super(command, description);
   }
-
 }
