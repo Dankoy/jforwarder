ae04edfb09804c53741b9c25c3a7d344bdd77237
Evgeny <transingularity@gmail.com>
2024-02-10 11:29:48 +0300

    refactor: commands holder has all commands by locale

--- telegram_bot/src/main/java/ru/dankoy/telegrambot/core/service/bot/commands/CommandsHolder.java
+++ telegram_bot/src/main/java/ru/dankoy/telegrambot/core/service/bot/commands/CommandsHolder.java
@@ -1,20 +1,39 @@
 package ru.dankoy.telegrambot.core.service.bot.commands;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import lombok.Getter;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
-
 
 @Getter
 public class CommandsHolder {
 
-  private final List<BotCommand> commands = new ArrayList<>();
+  private final Map<Locale, List<BotCommand>> commands = new HashMap<>();
 
-  public void addCommand(BotCommand command) {
+  public void addCommand(Locale locale, BotCommand botCommand) {
 
-    commands.add(command);
+    if (commands.containsKey(locale)) {
 
+      commands.get(locale).add(botCommand);
+
+    } else {
+      List<BotCommand> commandList = new ArrayList<>();
+      commandList.add(botCommand);
+      commands.put(locale, commandList);
+    }
   }
 
+  public void addCommands(Locale locale, List<BotCommand> botCommands) {
+
+    if (commands.containsKey(locale)) {
+
+      commands.get(locale).addAll(botCommands);
+
+    } else {
+      commands.put(locale, botCommands);
+    }
+  }
 }
