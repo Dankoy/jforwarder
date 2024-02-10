5e247a8caa8c8a51c8767820cad30791acfc779a
Evgeny <transingularity@gmail.com>
2024-02-10 11:32:57 +0300

    refactor: interface segregation for bot properties

--- telegram_bot/src/main/java/ru/dankoy/telegrambot/config/TelegramBotProperties.java
+++ telegram_bot/src/main/java/ru/dankoy/telegrambot/config/TelegramBotProperties.java
@@ -1,5 +1,6 @@
 package ru.dankoy.telegrambot.config;
 
+import java.util.Locale;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
@@ -9,9 +10,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Getter
 @RequiredArgsConstructor
 @ConfigurationProperties(prefix = "application.bot")
-public class TelegramBotProperties {
+public class TelegramBotProperties implements LocaleConfig, BotCredentialsConfig {
 
   private final String name;
   private final String token;
 
+  private final Locale[] locales;
+  private final Locale defaultLocale;
 }
