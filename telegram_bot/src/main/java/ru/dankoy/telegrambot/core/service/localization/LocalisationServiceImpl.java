8dbd17d11edaaef3eb954d525117e000766b601f
Evgeny <transingularity@gmail.com>
2024-02-10 11:31:14 +0300

    refactor: updated localization service

--- telegram_bot/src/main/java/ru/dankoy/telegrambot/core/service/localization/LocalisationServiceImpl.java
+++ telegram_bot/src/main/java/ru/dankoy/telegrambot/core/service/localization/LocalisationServiceImpl.java
@@ -5,18 +5,23 @@ import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
+import ru.dankoy.telegrambot.config.LocaleConfig;
 
 @Service
 @RequiredArgsConstructor
 public class LocalisationServiceImpl implements LocalisationService {
+
+  private final LocaleConfig localeConfig;
 
   private final MessageSource messageSource;
 
   @Override
   public String getLocalizedMessage(String code, Object[] objects, Locale locale) {
     Objects.requireNonNull(code);
-    Objects.requireNonNull(objects);
-    Objects.requireNonNull(locale);
+    if (locale == null) {
+      locale = localeConfig.getDefaultLocale();
+    }
+
     return messageSource.getMessage(code, objects, locale);
   }
 }
