a1c61c2eb73ab6fb3312987b98a4473d34eec043
Evgeny <transingularity@gmail.com>
2024-02-13 09:19:02 +0300

    refactor: updated migrations

--- subscriptions_holder/src/main/resources/db/migration/data/R__0009_update_orders.sql
+++ subscriptions_holder/src/main/resources/db/migration/data/R__0009_update_orders.sql
@@ -1,16 +1,59 @@
-WITH subquery AS (SELECT id
-                  FROM subscription_types)
-UPDATE orders
-SET type_id = subquery.id
-FROM subquery
-WHERE subquery.type = 'tag';
+-- insert for tag
+with select_order_type as (select id from subscription_types where type = 'tag'),
+     existing_order
+         as (select * from orders where id = 1)
+insert
+into order_subscription_type(order_id, subscription_type_id)
+values (existing_order.id, select_order_type.id);
+
+with select_order_type as (select id from subscription_types where type = 'tag'),
+     existing_order
+         as (select * from orders where id = 2)
+insert
+into order_subscription_type(order_id, subscription_type_id)
+values (existing_order.id, select_order_type.id);
+
+with select_order_type as (select id from subscription_types where type = 'tag'),
+     existing_order
+         as (select * from orders where id = 3)
+insert
+into order_subscription_type(order_id, subscription_type_id)
+values (existing_order.id, select_order_type.id);
+
+with select_order_type as (select id from subscription_types where type = 'tag'),
+     existing_order
+         as (select * from orders where id = 4)
+insert
+into order_subscription_type(order_id, subscription_type_id)
+values (existing_order.id, select_order_type.id);
 
 
-/*update
-    orders
-SET type_id = subquery.id
-FROM (SELECT id
-      FROM order_types
-      where type = 'tag') AS subquery
-;
-*/
+-- insert for channel
+with select_order_type as (select id from subscription_types where type = 'channel'),
+     new_order
+         as (insert into orders (name, value) values ('likes_count', 'most_liked') returning id)
+insert
+into order_subscription_type(order_id, subscription_type_id)
+values (new_order.id, select_order_type.id);
+
+with select_order_type as (select id from subscription_types where type = 'channel'),
+     new_order
+         as (insert into orders (name, value) values ('newest', 'most_recent') returning id)
+insert
+into order_subscription_type(order_id, subscription_type_id)
+values (new_order.id, select_order_type.id);
+
+with select_order_type as (select id from subscription_types where type = 'channel'),
+     new_order
+         as (insert into orders (name, value) values ('views_count', 'most_viewed') returning id)
+insert
+into order_subscription_type(order_id, subscription_type_id)
+values (new_order.id, select_order_type.id);
+
+with select_order_type as (select id from subscription_types where type = 'channel'),
+     new_order
+         as (insert into orders (name, value) values ('oldest', 'oldest') returning id)
+insert
+into order_subscription_type(order_id, subscription_type_id)
+values (new_order.id, select_order_type.id);
+
