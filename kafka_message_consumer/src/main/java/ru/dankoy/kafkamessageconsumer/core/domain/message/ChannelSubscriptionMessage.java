b7b3314dfca8ab9f4d96de2f6acba75045cf2c8f
Evgeny <transingularity@gmail.com>
2024-02-14 10:31:45 +0300

    refactor: added default constructor

--- kafka_message_consumer/src/main/java/ru/dankoy/kafkamessageconsumer/core/domain/message/ChannelSubscriptionMessage.java
+++ kafka_message_consumer/src/main/java/ru/dankoy/kafkamessageconsumer/core/domain/message/ChannelSubscriptionMessage.java
@@ -1,16 +1,18 @@
 package ru.dankoy.kafkamessageconsumer.core.domain.message;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
-import ru.dankoy.kafkamessageconsumer.core.domain.subscription.channelsubscription.Channel;
 import ru.dankoy.kafkamessageconsumer.core.domain.subscription.Order;
 import ru.dankoy.kafkamessageconsumer.core.domain.subscription.Scope;
 import ru.dankoy.kafkamessageconsumer.core.domain.subscription.Type;
+import ru.dankoy.kafkamessageconsumer.core.domain.subscription.channelsubscription.Channel;
 
 @Getter
 @SuperBuilder
 @ToString(callSuper = true)
+@NoArgsConstructor
 public final class ChannelSubscriptionMessage extends CoubMessage {
   private Channel channel;
 
