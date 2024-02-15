c74c33a9093a4e943dd986e12e8dbd00783e33c2
Evgeny <transingularity@gmail.com>
2024-02-15 08:58:14 +0300

    migrations: updated migrations

--- subscriptions_holder/src/main/resources/db/migration/data/R__0004_init_fixtures_tag_orders.sql
+++ subscriptions_holder/src/main/resources/db/migration/data/R__0004_init_fixtures_tag_orders.sql
@@ -1,7 +1,17 @@
 -- applied after 5.0 migration after renaming
-insert into orders(NAME)
-values ('likes_count'),
-       ('newest_popular'),
-       ('views_count'),
-       ('newest')
-on conflict do nothing;
+
+INSERT INTO orders(NAME, value)
+SELECT 'likes_count', 'top'
+WHERE NOT EXISTS (SELECT name, value FROM orders WHERE name = 'likes_count' and value = 'top');
+
+INSERT INTO orders(NAME, value)
+SELECT 'newest_popular', 'top'
+WHERE NOT EXISTS (SELECT name, value FROM orders WHERE name = 'newest_popular' and value = 'popular');
+
+INSERT INTO orders(NAME, value)
+SELECT 'views_count', 'views_count'
+WHERE NOT EXISTS (SELECT name, value FROM orders WHERE name = 'views_count' and value = 'views_count');
+
+INSERT INTO orders(NAME, value)
+SELECT 'newest', 'fresh'
+WHERE NOT EXISTS (SELECT name, value FROM orders WHERE name = 'newest' and value = 'fresh');
