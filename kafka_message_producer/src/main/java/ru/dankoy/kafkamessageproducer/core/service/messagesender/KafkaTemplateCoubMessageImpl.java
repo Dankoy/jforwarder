451a13f707ea125e07eb5e911a114cda9797aea6
Evgeny <transingularity@gmail.com>
2024-02-25 10:21:29 +0300

    refactor: removed producer ack

--- kafka_message_producer/src/main/java/ru/dankoy/kafkamessageproducer/core/service/messagesender/KafkaTemplateCoubMessageImpl.java
+++ kafka_message_producer/src/main/java/ru/dankoy/kafkamessageproducer/core/service/messagesender/KafkaTemplateCoubMessageImpl.java
@@ -1,6 +1,5 @@
 package ru.dankoy.kafkamessageproducer.core.service.messagesender;
 
-import java.util.function.Consumer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -13,10 +12,6 @@ public class KafkaTemplateCoubMessageImpl implements KafkaTemplateCoubMessage {
 
   private final KafkaTemplate<String, CoubMessage> kafkaTemplate;
 
-  private final Consumer<CoubMessage> sendAck;
-
-  private final Consumer<CoubMessage> sendAckToRegistry;
-
   public void send(ProducerRecord<String, CoubMessage> producerRecord) {
 
     kafkaTemplate
@@ -28,11 +23,6 @@ public class KafkaTemplateCoubMessageImpl implements KafkaTemplateCoubMessage {
                     "message id: {} was sent, offset: {}",
                     producerRecord.value().getId(),
                     result.getRecordMetadata().offset());
-                // if kafka accepted - send update last permalink in db for
-                // subscription
-                sendAck.accept(producerRecord.value());
-                // update registry
-                sendAckToRegistry.accept(producerRecord.value());
                 log.info("acknowledgement sent for {}", producerRecord.value());
               } else {
                 log.error("message id:{} was not sent", producerRecord.value().getId(), ex);
