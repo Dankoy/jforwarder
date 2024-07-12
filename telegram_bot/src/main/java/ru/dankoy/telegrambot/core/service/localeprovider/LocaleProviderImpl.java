61ab209fa4703221ab44ccba94985a0567095934
Evgeny <transingularity@gmail.com>
2024-07-12 18:57:51 +0300

    fix: added check when locale is null (#121)
    * fix: added check when locale is null
    
    * chore: updated release version to 1.3.3
    
    * refactor: moved duplicate to private method

--- telegram_bot/src/main/java/ru/dankoy/telegrambot/core/service/localeprovider/LocaleProviderImpl.java
+++ telegram_bot/src/main/java/ru/dankoy/telegrambot/core/service/localeprovider/LocaleProviderImpl.java
@@ -1,19 +1,37 @@
 package ru.dankoy.telegrambot.core.service.localeprovider;
 
 import java.util.Locale;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Message;
+import ru.dankoy.telegrambot.config.bot.properties.LocaleConfig;
 
 @Service
+@RequiredArgsConstructor
 public class LocaleProviderImpl implements LocaleProvider {
+
+  private final LocaleConfig localeConfig;
 
   @Override
   public Locale getLocale(Message message) {
-    return Locale.forLanguageTag(message.getFrom().getLanguageCode());
+
+    var langCode = message.getFrom().getLanguageCode();
+
+    return checkAndReturn(langCode);
   }
 
   @Override
   public Locale getLocale(String locale) {
-    return Locale.forLanguageTag(locale);
+
+    return checkAndReturn(locale);
+  }
+
+  private Locale checkAndReturn(String locale) {
+    if (Objects.isNull(locale)) {
+      return localeConfig.getDefaultLocale();
+    } else {
+      return Locale.of(locale);
+    }
   }
 }
