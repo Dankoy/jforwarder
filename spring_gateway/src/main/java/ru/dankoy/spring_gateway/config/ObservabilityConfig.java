d56673e6df4e7e358e0d5af5896d5afb62e0807e
Evgeny <transingularity@gmail.com>
2024-07-13 12:30:57 +0300

    refactor: exclude eureka requests from zipkin (#122)
    * refactor: exclude eureka requests from zipkin
    
    * refactor: exclude outgoing reqs to eureka from zipkin
    
    * chore: updated version to 1.3.4

--- spring_gateway/src/main/java/ru/dankoy/spring_gateway/config/ObservabilityConfig.java
+++ spring_gateway/src/main/java/ru/dankoy/spring_gateway/config/ObservabilityConfig.java
@@ -4,6 +4,7 @@ import io.micrometer.observation.ObservationRegistry;
 import org.springframework.boot.actuate.autoconfigure.observation.ObservationRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.observation.ClientRequestObservationContext;
 import org.springframework.http.server.reactive.observation.ServerRequestObservationContext;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.util.PathMatcher;
@@ -36,5 +37,21 @@ public class ObservabilityConfig {
         registry
             .observationConfig()
             .observationPredicate((name, context) -> !name.startsWith("spring.security"));
+  }
+
+  @Bean
+  ObservationRegistryCustomizer<ObservationRegistry> skipEurekaRegistrySpansFromObservation() {
+    // this excludes output requests from service made by some rest client.
+    return registry ->
+        registry
+            .observationConfig()
+            .observationPredicate(
+                (name, context) -> {
+                  if (context instanceof ClientRequestObservationContext serverContext) {
+                    return !serverContext.getCarrier().getURI().getPath().startsWith("/eureka");
+                  } else {
+                    return true;
+                  }
+                });
   }
 }
