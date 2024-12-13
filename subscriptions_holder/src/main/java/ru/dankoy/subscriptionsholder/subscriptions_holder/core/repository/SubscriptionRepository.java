b4313a40862cd21a101e5d887133a31bb9bb938e
Evgeny <transingularity@gmail.com>
2024-12-13 13:01:47 +0300

    refactor: added select for update method

--- subscriptions_holder/src/main/java/ru/dankoy/subscriptionsholder/subscriptions_holder/core/repository/SubscriptionRepository.java
+++ subscriptions_holder/src/main/java/ru/dankoy/subscriptionsholder/subscriptions_holder/core/repository/SubscriptionRepository.java
@@ -1,6 +1,16 @@
 package ru.dankoy.subscriptionsholder.subscriptions_holder.core.repository;
 
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.QueryHints;
 import ru.dankoy.subscriptionsholder.subscriptions_holder.core.domain.subscriptions.Subscription;
 
-public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {}
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  @QueryHints({@QueryHint(name = "jakarta.persistence.lock.timeout", value = "3000")})
+  Optional<Subscription> findForUpdateById(long id);
+}
