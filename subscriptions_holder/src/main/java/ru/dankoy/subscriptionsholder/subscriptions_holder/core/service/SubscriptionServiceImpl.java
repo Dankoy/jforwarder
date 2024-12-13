e29432b259832c93f9f2cb573a86f51d77aa1613
Evgeny <transingularity@gmail.com>
2024-12-13 13:02:06 +0300

    refactor: permalink update through select for update

--- subscriptions_holder/src/main/java/ru/dankoy/subscriptionsholder/subscriptions_holder/core/service/SubscriptionServiceImpl.java
+++ subscriptions_holder/src/main/java/ru/dankoy/subscriptionsholder/subscriptions_holder/core/service/SubscriptionServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.dankoy.subscriptionsholder.subscriptions_holder.core.service;
 
 import jakarta.transaction.Transactional;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.dankoy.subscriptionsholder.subscriptions_holder.core.domain.subscriptions.Subscription;
@@ -18,16 +19,23 @@ public class SubscriptionServiceImpl implements SubscriptionService {
 
     var optional = subscriptionRepository.findById(id);
 
-    return optional.orElseThrow(
-        () -> new ResourceNotFoundException(String.format("Subscription not found: %s", id)));
+    return checkOptional(optional, id);
   }
 
   @Transactional
   @Override
   public Subscription updatePermalink(Subscription subscription) {
 
-    var sub = findById(subscription.getId());
+    var optionalSub = subscriptionRepository.findForUpdateById(subscription.getId());
+    var sub = checkOptional(optionalSub, subscription.getId());
+
     sub.setLastPermalink(subscription.getLastPermalink());
     return subscriptionRepository.save(sub);
+  }
+
+  private Subscription checkOptional(Optional<Subscription> optional, long id) {
+
+    return optional.orElseThrow(
+        () -> new ResourceNotFoundException(String.format("Subscription not found: %s", id)));
   }
 }
