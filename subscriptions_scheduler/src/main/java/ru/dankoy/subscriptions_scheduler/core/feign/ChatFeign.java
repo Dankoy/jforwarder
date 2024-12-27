df423a1493f9ea80fbdda768dc0a0646c1c9763e
Evgeny <transingularity@gmail.com>
2024-12-27 10:10:43 +0300

    refactor: updated feign

--- subscriptions_scheduler/src/main/java/ru/dankoy/subscriptions_scheduler/core/feign/ChatFeign.java
+++ subscriptions_scheduler/src/main/java/ru/dankoy/subscriptions_scheduler/core/feign/ChatFeign.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import ru.dankoy.subscriptions_scheduler.core.domain.subscribtionsholder.Chat;
+import ru.dankoy.subscriptions_scheduler.core.dto.subscriptions.ChatDTO;
 
 @FeignClient(contextId = "telegram_chat", name = "subscriptions-holder")
 public interface ChatFeign {
@@ -13,5 +14,5 @@ public interface ChatFeign {
   @GetMapping(
       path = "/api/v1/telegram_chat",
       params = {"page", "size", "sort"})
-  Page<Chat> getAllChats(@PathVariable Pageable pageable);
+  Page<ChatDTO> getAllChats(Pageable pageable);
 }
--- subscriptions_scheduler/src/main/java/ru/dankoy/subscriptions_scheduler/core/feign/SubscriptionsFeign.java
+++ subscriptions_scheduler/src/main/java/ru/dankoy/subscriptions_scheduler/core/feign/SubscriptionsFeign.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import ru.dankoy.subscriptions_scheduler.core.domain.subscribtionsholder.Chat;
+import ru.dankoy.subscriptions_scheduler.core.dto.subscriptions.SubscriptionDTO;
 
 @FeignClient(contextId = "subscriptions", name = "subscriptions-holder")
 public interface SubscriptionsFeign {
@@ -13,5 +14,5 @@ public interface SubscriptionsFeign {
   @GetMapping(
       path = "/api/v1/subscriptions",
       params = {"page", "size", "sort"})
-  Page<Chat> getAllChats(@PathVariable Pageable pageable);
+  Page<SubscriptionDTO> getAllChats(Pageable pageable);
 }
