2fcf9b6fb86bce22ac8a6acdb1363f5be0bb07e3
Evgeny <transingularity@gmail.com>
2024-12-27 10:11:36 +0300

    refactor: added dtos

---
+++ subscriptions_scheduler/src/main/java/ru/dankoy/subscriptions_scheduler/core/dto/subscriptions/ChatDTO.java
@@ -0,0 +1,26 @@
+package ru.dankoy.subscriptions_scheduler.core.dto.subscriptions;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+public class ChatDTO {
+
+  @Min(value = 1, message = "ID must be greater than 0")
+  private long id;
+
+  @NotNull(message = "Chat ID must not be null")
+  private long chatId;
+
+  @NotNull(message = "Active status must not be null")
+  private boolean active;
+
+  private Integer messageThreadId;
+
+  @NotNull(message = "Date created must not be null")
+  private LocalDateTime dateCreated;
+
+  private LocalDateTime dateModified;
+}
---
+++ subscriptions_scheduler/src/main/java/ru/dankoy/subscriptions_scheduler/core/dto/subscriptions/SubscriptionDTO.java
@@ -0,0 +1,22 @@
+package ru.dankoy.subscriptions_scheduler.core.dto.subscriptions;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+public class SubscriptionDTO {
+
+  @Min(value = 1, message = "Subscription ID must be greater than 0")
+  private long id;
+
+  @NotNull(message = "Chat must not be null")
+  private ChatDTO chat;
+
+  @NotNull(message = "CreatedAt must not be null")
+  private LocalDateTime createdAt;
+
+  @NotNull(message = "ModifiedAt must not be null")
+  private LocalDateTime modifiedAt;
+}
