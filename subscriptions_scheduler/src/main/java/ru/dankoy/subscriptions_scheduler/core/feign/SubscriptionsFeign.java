4154e1cfb0b4a04cb32b0509715c2a0c15489017
Evgeny <transingularity@gmail.com>
2024-12-27 10:11:43 +0300

    style: style

--- subscriptions_scheduler/src/main/java/ru/dankoy/subscriptions_scheduler/core/feign/SubscriptionsFeign.java
+++ subscriptions_scheduler/src/main/java/ru/dankoy/subscriptions_scheduler/core/feign/SubscriptionsFeign.java
@@ -4,8 +4,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import ru.dankoy.subscriptions_scheduler.core.domain.subscribtionsholder.Chat;
 import ru.dankoy.subscriptions_scheduler.core.dto.subscriptions.SubscriptionDTO;
 
 @FeignClient(contextId = "subscriptions", name = "subscriptions-holder")
