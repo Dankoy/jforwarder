1384ab27cdfea39bfca9ae2d90d415843b6b77fa
Evgeny <transingularity@gmail.com>
2025-01-04 10:49:47 +0300

    fix: probably fix cwe-1333

--- subscriptions_holder/src/main/java/ru/dankoy/subscriptionsholder/subscriptions_holder/core/service/searchparser/SearchCriteriaParserImpl.java
+++ subscriptions_holder/src/main/java/ru/dankoy/subscriptionsholder/subscriptions_holder/core/service/searchparser/SearchCriteriaParserImpl.java
@@ -13,13 +13,24 @@ public class SearchCriteriaParserImpl implements SearchCriteriaParser {
   @Override
   public List<SearchCriteria> parse(String search) {
     List<SearchCriteria> params = new ArrayList<>();
-    if (search != null) {
-      Pattern pattern = Pattern.compile("(\\w+?)(:|<|>)(\\w+?),");
-      Matcher matcher = pattern.matcher(search + ",");
-      while (matcher.find()) {
-        params.add(new SearchCriteria(matcher.group(1), matcher.group(2), matcher.group(3)));
-      }
+
+    if (search == null || !isValidString(search)) {
+      throw new IllegalArgumentException("Invalid search criteria");
     }
+
+    search = search + ",";
+
+    Pattern pattern = Pattern.compile("(\\w+?)([:<>])(\\w+?),");
+    Matcher matcher = pattern.matcher(search);
+
+    while (matcher.find()) {
+      params.add(new SearchCriteria(matcher.group(1), matcher.group(2), matcher.group(3)));
+    }
+
     return params;
+  }
+
+  private boolean isValidString(String str) {
+    return str != null && !str.isEmpty() && str.matches("[a-zA-Z0-9][:<>],");
   }
 }
