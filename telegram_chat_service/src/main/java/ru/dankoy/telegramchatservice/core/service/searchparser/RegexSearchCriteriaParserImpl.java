f38bc8f34d53bb555157cbf0e989b4428082e997
Evgeny <transingularity@gmail.com>
2025-03-02 09:30:10 +0300

    refactor: removed search criteria parser

--- telegram_chat_service/src/main/java/ru/dankoy/telegramchatservice/core/service/searchparser/RegexSearchCriteriaParserImpl.java
+++ telegram_chat_service/src/main/java/ru/dankoy/telegramchatservice/core/service/searchparser/RegexSearchCriteriaParserImpl.java
@@ -2,8 +2,6 @@ package ru.dankoy.telegramchatservice.core.service.searchparser;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import org.springframework.stereotype.Service;
 import ru.dankoy.telegramchatservice.core.domain.search.RegexSearchCriteria;
 
@@ -13,13 +11,14 @@ public class RegexSearchCriteriaParserImpl implements RegexSearchCriteriaParser 
   @Override
   public List<RegexSearchCriteria> parse(String search) {
     List<RegexSearchCriteria> params = new ArrayList<>();
-    if (search != null) {
-      Pattern pattern = Pattern.compile("(\\w+?)(:|<|>)(\\w+?),");
-      Matcher matcher = pattern.matcher(search + ",");
-      while (matcher.find()) {
-        params.add(new RegexSearchCriteria(matcher.group(1), matcher.group(2), matcher.group(3)));
-      }
-    }
+    // if (search != null) {
+    //   Pattern pattern = Pattern.compile("(\\w+?)(:|<|>)(\\w+?),");
+    //   Matcher matcher = pattern.matcher(search + ",");
+    //   while (matcher.find()) {
+    //     params.add(new RegexSearchCriteria(matcher.group(1), matcher.group(2),
+    // matcher.group(3)));
+    //   }
+    // }
     return params;
   }
 }
