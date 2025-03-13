372cd38715bb7802baee75bd6318f25101ba482f
Evgeny <transingularity@gmail.com>
2025-03-13 12:47:42 +0300

    test: fix time precision to 6 digits

--- telegram_chat_service/src/test/java/ru/dankoy/telegramchatservice/core/service/ChatMaker.java
+++ telegram_chat_service/src/test/java/ru/dankoy/telegramchatservice/core/service/ChatMaker.java
@@ -1,6 +1,7 @@
 package ru.dankoy.telegramchatservice.core.service;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 import ru.dankoy.telegramchatservice.core.domain.Chat;
 import ru.dankoy.telegramchatservice.core.domain.dto.ChatDTO;
@@ -8,15 +9,19 @@ import ru.dankoy.telegramchatservice.core.domain.dto.ChatDTO;
 public interface ChatMaker {
 
   default Chat makeChat(long chatId) {
-    var date = LocalDateTime.now();
-
+    DateTimeFormatter formatter =
+    DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss.SSSSSS");
+    var date = LocalDateTime.parse(LocalDateTime.now().format(formatter), formatter);
+    
     return new Chat(
         0L, chatId, "type", "title", "firstName", "lastName", "username", true, 1, date, date);
   }
 
   default ChatDTO makeChat(UUID id, long chatId) {
-    var date = LocalDateTime.now();
-
+    DateTimeFormatter formatter =
+    DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss.SSSSSS");
+    var date = LocalDateTime.parse(LocalDateTime.now().format(formatter), formatter);
+    
     return new ChatDTO(
         id, chatId, "type", "title", "firstName", "lastName", "username", true, 1, date, date);
   }
