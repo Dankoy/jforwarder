980ce89c81ae0897d1fd5de6e55f310c9ab3238a
Evgeny <transingularity@gmail.com>
2025-03-13 12:48:01 +0300

    test: style

--- telegram_chat_service/src/test/java/ru/dankoy/telegramchatservice/core/service/ChatMaker.java
+++ telegram_chat_service/src/test/java/ru/dankoy/telegramchatservice/core/service/ChatMaker.java
@@ -9,19 +9,17 @@ import ru.dankoy.telegramchatservice.core.domain.dto.ChatDTO;
 public interface ChatMaker {
 
   default Chat makeChat(long chatId) {
-    DateTimeFormatter formatter =
-    DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss.SSSSSS");
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss.SSSSSS");
     var date = LocalDateTime.parse(LocalDateTime.now().format(formatter), formatter);
-    
+
     return new Chat(
         0L, chatId, "type", "title", "firstName", "lastName", "username", true, 1, date, date);
   }
 
   default ChatDTO makeChat(UUID id, long chatId) {
-    DateTimeFormatter formatter =
-    DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss.SSSSSS");
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss.SSSSSS");
     var date = LocalDateTime.parse(LocalDateTime.now().format(formatter), formatter);
-    
+
     return new ChatDTO(
         id, chatId, "type", "title", "firstName", "lastName", "username", true, 1, date, date);
   }
