c0c791a5e019d21846130f3fbba48c3100975675
Evgeny <transingularity@gmail.com>
2025-03-13 12:53:07 +0300

    Fix unaccessibel group chats (#191)
    * fix: fixed condition to work with nulls
    
    * refactor: added transactional annotations
    
    * fix: bug with unaccessible group chats
    
    * test: added jooq logs in tests
    
    * test: added sout to find out why tests doesn't pass
    
    * test: removed sout
    
    * test: fix time precision to 6 digits
    
    * test: style

--- telegram_chat_service/src/main/java/ru/dankoy/telegramchatservice/core/repository/TelegramChatDaoJooq.java
+++ telegram_chat_service/src/main/java/ru/dankoy/telegramchatservice/core/repository/TelegramChatDaoJooq.java
@@ -137,8 +137,19 @@ public class TelegramChatDaoJooq implements TelegramChatDao {
   @Override
   public Optional<ChatDTO> findByChatIdAndMessageThreadId(long chatId, Integer threadId) {
 
-    var condition = noCondition();
-    condition = condition.and(CHATS.CHAT_ID.eq(chatId)).and(CHATS.MESSAGE_THREAD_ID.eq(threadId));
+    var conditionLeft = noCondition();
+    var conditionRight = noCondition();
+
+    conditionLeft =
+        conditionLeft.and(CHATS.CHAT_ID.eq(chatId)).and(CHATS.MESSAGE_THREAD_ID.eq(threadId));
+
+    conditionRight =
+        conditionRight
+            .and(CHATS.CHAT_ID.eq(chatId))
+            .and(CHATS.MESSAGE_THREAD_ID.isNull())
+            .and(val(threadId).isNull());
+
+    var condition = or(conditionLeft, conditionRight);
 
     ChatsRecord chatsRecord = dsl.fetchOne(CHATS, condition);
 
@@ -150,8 +161,19 @@ public class TelegramChatDaoJooq implements TelegramChatDao {
   @Override
   public Optional<ChatDTO> findForUpdateByChatIdAndMessageThreadId(long chatId, Integer threadId) {
 
-    var condition = noCondition();
-    condition = condition.and(CHATS.CHAT_ID.eq(chatId)).and(CHATS.MESSAGE_THREAD_ID.eq(threadId));
+    var conditionLeft = noCondition();
+    var conditionRight = noCondition();
+
+    conditionLeft =
+        conditionLeft.and(CHATS.CHAT_ID.eq(chatId)).and(CHATS.MESSAGE_THREAD_ID.eq(threadId));
+
+    conditionRight =
+        conditionRight
+            .and(CHATS.CHAT_ID.eq(chatId))
+            .and(CHATS.MESSAGE_THREAD_ID.isNull())
+            .and(val(threadId).isNull());
+
+    var condition = or(conditionLeft, conditionRight);
 
     Optional<Record> chatsRecord =
         dsl.select().from(CHATS).where(condition).forUpdate().wait(3).fetchOptional();
