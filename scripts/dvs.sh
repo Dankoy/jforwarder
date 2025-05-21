0acf948b49fca586c3ce87d3c9f92aaa3f7bd3b7
Evgeny <transingularity@gmail.com>
2025-05-21 12:46:34 +0300

    refactor: fix style

--- scripts/dvs.sh
+++ scripts/dvs.sh
@@ -28,32 +28,33 @@ function isVolumeExists {
 }
 
 # check if old volume exists
-
-if ! isVolumeExists "${OLD_VOLUME_NAME}"
+if isVolumeExists "${OLD_VOLUME_NAME}" -eq 0 ;
 then
     echo "Volume $OLD_VOLUME_NAME doesn't exist"
     exit 2
 fi
 
 # check if new volume exists
-
-if ! isVolumeExists "${NEW_VOLUME_NAME}"
+if isVolumeExists "${NEW_VOLUME_NAME}" -eq 0;
 then
     echo "creating '$NEW_VOLUME_NAME' ..."
     docker volume create "${NEW_VOLUME_NAME}" 2>/dev/null 1>/dev/null
     isVolumeExists "${NEW_VOLUME_NAME}"
-    if [[ "$?" -eq 0 ]]; then
+    if isVolumeExists "${NEW_VOLUME_NAME}" -eq 0; then
         echo "Cannot create new volume"
         exit 3
     else
         echo "OK"
     fi
+else 
+    echo "volume '$NEW_VOLUME_NAME' is already exist. Can't copy anything to it."
+    exit 3
 fi
 
 # most important part, data migration
 #docker run --rm --volume "${OLD_VOLUME_NAME}":/source --volume "${NEW_VOLUME_NAME}":/destination ubuntu:latest bash -c "echo 'copying volume ...'; cp -Rp /source/* /destination/"
-
-if ! docker run --rm --volume "${OLD_VOLUME_NAME}":/source --volume "${NEW_VOLUME_NAME}":/destination ubuntu:latest bash -c "echo 'copying volume ...'; cp -Rp /source/* /destination/"
+#if ! func; then - to check for negative evaluation first
+if docker run --rm --volume "${OLD_VOLUME_NAME}":/source --volume "${NEW_VOLUME_NAME}":/destination ubuntu:latest bash -c "echo 'copying volume ...'; cp -Rp /source/* /destination/";
 then
     echo "Done successfuly 🎉"
 else
