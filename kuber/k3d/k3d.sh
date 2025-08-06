31e38127d3f2a171e26cb7eb7119a36bf1b44f09
Evgeny <transingularity@gmail.com>
2025-08-06 10:50:45 +0300

    chore: added script to apply namespaces and context for k3d

---
+++ kuber/k3d/k3d.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+## creates namespaces for development and production environments
+
+kubectl apply -f namespaces/jforwarder-namespace.yaml
+kubectl apply -f namespaces/kafka-namespace.yaml
+kubectl apply -f namespaces/monitoring-namespace.yaml
+kubectl apply -f namespaces/minio-namespace.yaml
+kubectl apply -f namespaces/mimir-namespace.yaml
+
+  kubectl config set-context jforwarder --namespace=jforwarder \
+  --cluster=k3d-mycluster \
+  --user=admin@k3d-mycluster
+
+kubectl config set-context kafka --namespace=kafka \
+  --cluster=k3d-mycluster \
+  --user=admin@k3d-mycluster
+
+kubectl config set-context monitoring --namespace=monitoring \
+  --cluster=k3d-mycluster \
+  --user=admin@k3d-mycluster
+
+kubectl config set-context minio --namespace=minio \
+  --cluster=k3d-mycluster \
+  --user=admin@k3d-mycluster
+
+kubectl config set-context mimir --namespace=mimir \
+  --cluster=k3d-mycluster \
+  --user=admin@k3d-mycluster
