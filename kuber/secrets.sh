50f578e725dcb66e68c49f4119614c9168edf845
Evgeny <transingularity@gmail.com>
2025-08-06 21:40:54 +0300

    chore: added script to fastly copy secrets

---
+++ kuber/secrets.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## copy secret files instead of dummy files in repo
+
+cp .all_secrets/project/secrets/* project/secrets/
+cp .all_secrets/monitoring/grafana/secrets/* monitoring/grafana/secrets/
+cp .all_secrets/monitoring/mimir/mimir-secret.yaml monitoring/mimir/mimir-secret.yaml
+cp .all_secrets/monitoring/minio/minio-env-secret.yaml monitoring/mimir/minio-env-secret.yaml
+
