57e262e89824bdd2f5846e30812a3bab30c69260
Evgeny <transingularity@gmail.com>
2025-08-30 16:19:36 +0300

    refactor: added copy alertmanager secrets (#257)

--- kuber/secrets.sh
+++ kuber/secrets.sh
@@ -6,4 +6,5 @@ cp .all_secrets/project/secrets/* project/secrets/
 cp .all_secrets/monitoring/grafana/secrets/* monitoring/grafana/secrets/
 cp .all_secrets/monitoring/mimir/mimir-secret.yaml monitoring/mimir/mimir-secret.yaml
 cp .all_secrets/monitoring/minio/minio-env-secret.yaml monitoring/minio/minio-env-secret.yaml
+cp .all_secrets/monitoring/alertmanager/secrets/telegram-bot-token-secret.yaml monitoring/alertmanager/secrets/telegram-bot-token-secret.yaml
 
