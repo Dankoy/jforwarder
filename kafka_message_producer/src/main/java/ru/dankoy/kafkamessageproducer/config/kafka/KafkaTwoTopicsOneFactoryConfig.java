2b7c7161eff06ba64e97311e86c463d2ffe77ca2
Evgeny <transingularity@gmail.com>
2026-01-02 12:51:13 +0300

    refactor: updated kafka producer

--- kafka_message_producer/src/main/java/ru/dankoy/kafkamessageproducer/config/kafka/KafkaTwoTopicsOneFactoryConfig.java
+++ kafka_message_producer/src/main/java/ru/dankoy/kafkamessageproducer/config/kafka/KafkaTwoTopicsOneFactoryConfig.java
@@ -1,21 +1,18 @@
 package ru.dankoy.kafkamessageproducer.config.kafka;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.charset.StandardCharsets;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
-import org.springframework.boot.ssl.SslBundles;
+import org.springframework.boot.kafka.autoconfigure.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.JacksonUtils;
-import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
 import ru.dankoy.kafkamessageproducer.core.domain.message.CoubMessage;
 import ru.dankoy.kafkamessageproducer.core.service.messagesender.generic.KafkaTemplateGeneric;
 import ru.dankoy.kafkamessageproducer.core.service.messagesender.generic.KafkaTemplateGenericImpl;
@@ -37,16 +34,10 @@ public class KafkaTwoTopicsOneFactoryConfig {
   }
 
   @Bean
-  public ObjectMapper objectMapper() {
-    return JacksonUtils.enhancedObjectMapper();
-  }
-
-  @Bean
-  public ProducerFactory<String, CoubMessage> producerFactory(
-      KafkaProperties kafkaProperties, ObjectMapper mapper, SslBundles sslBundles) {
-    var props = kafkaProperties.buildProducerProperties(sslBundles);
+  public ProducerFactory<String, CoubMessage> producerFactory(KafkaProperties kafkaProperties) {
+    var props = kafkaProperties.buildProducerProperties();
     props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
     props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
     props.put(ProducerConfig.ACKS_CONFIG, "all");
     props.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, 5000);
@@ -57,7 +48,7 @@ public class KafkaTwoTopicsOneFactoryConfig {
     props.put(ProducerConfig.CLIENT_ID_CONFIG, communityProducerClientId);
 
     var kafkaProducerFactory = new DefaultKafkaProducerFactory<String, CoubMessage>(props);
-    kafkaProducerFactory.setValueSerializer(new JsonSerializer<>(mapper));
+    kafkaProducerFactory.setValueSerializer(new JacksonJsonSerializer<CoubMessage>());
     return kafkaProducerFactory;
   }
 
