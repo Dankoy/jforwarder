5b7e834707baa4e683383dcf0583d9b71ed55a25
Evgeny <transingularity@gmail.com>
2026-09-20 00:45:26 +0300

    fix: type sql

--- subscriptions_holder/src/main/resources/db/migration/9.0/V9.1__2026_09_19_increase_permalink_varchar_limits.sql
+++ subscriptions_holder/src/main/resources/db/migration/9.0/V9.1__2026_09_19_increase_permalink_varchar_limits.sql
@@ -1,7 +1,7 @@
 -- Active tables 
-alter table subscriptions alter column last_permalink varchar(30);
-alter table sent_coubs_registry alter column coub_permalink varchar(30) not null;
+alter table subscriptions alter column last_permalink type varchar(30);
+alter table sent_coubs_registry alter column coub_permalink type varchar(30) not null;
 
 -- Not active tables, for removal
-alter table community_subscriptions alter column last_permalink varchar(30);
-alter table tag_subscriptions alter column last_permalink varchar(30);
+alter table community_subscriptions alter column last_permalink type varchar(30);
+alter table tag_subscriptions alter column last_permalink type varchar(30);
