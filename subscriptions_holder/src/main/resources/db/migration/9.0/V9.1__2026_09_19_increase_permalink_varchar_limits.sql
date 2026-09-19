016656c4d6a7162a02d1545270dd60537d24e5e2
Evgeny <transingularity@gmail.com>
2026-09-20 01:28:49 +0300

    refactor: permalink increase varchar from 10 to 30 (#375)
    * refactor: increased varchar limit to 30
    
    * fix: type sql
    
    * fix: not null is not necessary, it keeps

---
+++ subscriptions_holder/src/main/resources/db/migration/9.0/V9.1__2026_09_19_increase_permalink_varchar_limits.sql
@@ -0,0 +1,7 @@
+-- Active tables 
+alter table subscriptions alter column last_permalink type varchar(30);
+alter table sent_coubs_registry alter column coub_permalink type varchar(30);
+
+-- Not active tables, for removal
+alter table community_subscriptions alter column last_permalink type varchar(30);
+alter table tag_subscriptions alter column last_permalink type varchar(30);
